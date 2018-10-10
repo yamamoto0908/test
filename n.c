e7c0954776c05d16628f4ecb0ec77ff36a8a0386
Tatsuya Yamamoto <k16jk602@st.kyusan-u.ac.jp>
2018-10-10 14:15:02 +0900

    Update n.c

---
+++ n.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+
+double sqrt(double x){
+  
+  double tmp;
+  tmp =sqrt(x);
+  return(tmp);
+}
+int main(void){
+  
+  int n;
+  double x;
+
+  printf("ｎの値：");
+  scanf("%d",&sqrt(x));
+  
+}
+
